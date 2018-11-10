c4b7a9da635ae0b4806450121b36848b1126929a
pat <patlehmann1@gmail.com>
2018-11-09 21:24:53 -0500

    Updates

---
+++ precache-manifest.0a3ecc5af35c38c1bc63e44555f1e503.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "053e654c366bc0d6c4003c3e3c2e4e71",
+    "url": "/react_portfolio/static/media/coding2.053e654c.jpg"
+  },
+  {
+    "revision": "c37a5afe0dc8109e461b",
+    "url": "/react_portfolio/static/js/runtime~main.c37a5afe.js"
+  },
+  {
+    "revision": "397a7ecd356fef517da7",
+    "url": "/react_portfolio/static/js/main.397a7ecd.chunk.js"
+  },
+  {
+    "revision": "0cdd6f184db929cffcf5",
+    "url": "/react_portfolio/static/js/1.0cdd6f18.chunk.js"
+  },
+  {
+    "revision": "397a7ecd356fef517da7",
+    "url": "/react_portfolio/static/css/main.800d9423.chunk.css"
+  },
+  {
+    "revision": "7ed532833d715a8d8fe14defc3a0e2af",
+    "url": "/react_portfolio/index.html"
+  }
+];
