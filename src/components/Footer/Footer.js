4723cecd6e8f6132d604e2b7ea3d33c1d8426116
pat <patlehmann1@gmail.com>
2018-11-12 22:39:07 -0500

    fixed responsiveness issue

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className="app_footer text-white">
             <p className="footer_text">Made with love by
-            <a className="contact_link" href="github.com/patlehmann1/"
+            <a className="contact_link" href="https://github.com/patlehmann1/"
                     target="_blank" rel="noopener noreferrer"> Patrick Lehmann
             </a>, late 2018.
             </p>
